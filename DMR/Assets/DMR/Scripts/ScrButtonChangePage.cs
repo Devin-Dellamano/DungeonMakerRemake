bc985183591b5a6d8af9d4846e02b7db16450bee
Maikikia13 <maikikia@gmail.com>
2018-10-13 17:43:31 -0500

    Committing the start menu and the character select screen.

---
+++ DMR/Assets/DMR/Scripts/ScrButtonChangePage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrButtonChangePage : MonoBehaviour {
+
+    public GameObject characterSelect;
+    public List<GameObject> buttons;
+
+    public void ChangePanel(string nameOfPanel)
+    {
+        characterSelect.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
