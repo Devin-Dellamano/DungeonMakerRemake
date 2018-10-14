9c6435cf4870b2d43f2f5fc3efd9c8b2159ee2c3
Devin-Dellamano <devin_dellamano@yahoo.com>
2018-10-14 11:05:45 -0500

    Dark Lord Evie now starts idle and attacks when clicked.

--- DMR/Assets/DMR/Data/DarkLordData/ScrPlayerSkillActivation.cs
+++ DMR/Assets/DMR/Data/DarkLordData/ScrPlayerSkillActivation.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrPlayerSkillActivation : MonoBehaviour {
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetMouseButtonUp(0))
         {
-            gameObject.GetComponent<Animator>().Play("NormalATK");
+            anim.SetBool("Attacking", true);
+            anim.SetBool("Idle", false);
             Debug.Log("Clicked");
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2"))
+        {
+            anim.SetBool("Attacking", false);
+            anim.SetBool("Idle", true);
+            anim.Play("Idle");
         }
     }
 }
