b035ad2e5fc6d677d90cbc1ec2db25bb100632aa
Maikikia13 <maikikia@gmail.com>
2018-10-13 19:16:58 -0500

    Jeff sounds added to main menu and new game buttons

--- DMR/Assets/DMR/Scripts/ScrButtonChangePage.cs
+++ DMR/Assets/DMR/Scripts/ScrButtonChangePage.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrButtonChangePage : MonoBehaviour {
+public class ScrButtonChangePage : MonoBehaviour
+{
 
     public GameObject characterSelect;
     public List<GameObject> buttons;
+
+    public AudioSource sourceSound;
+    public AudioClip clipSound;
+
+    public void ChangePanelWithSound(string nameOfPanel)
+    {
+        characterSelect.SetActive(true);
+        this.gameObject.SetActive(false);
+        sourceSound.Play();
+    }
 
     public void ChangePanel(string nameOfPanel)
     {
